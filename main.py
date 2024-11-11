10da086a654b765001920b799f90d4a16b08acb7
clickTwice26 <shagatoc@gmail.com>
2024-11-11 20:18:03 +0800

    initialized project in server

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database/agricooo.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database/neobank3.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
